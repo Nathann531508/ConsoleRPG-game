dea934f5edcb00f6e5216697eb6d757bb94f4cdc
Nathann531508 <113695188+Nathann531508@users.noreply.github.com>
2022-09-26 13:58:37 +0100

    Add files via upload

---
+++ CSharp2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
+
+
+Console.WriteLine("Welcome to my game type Start to begin");
+
+/*
+ Console.ReadLine();
+*/
+
